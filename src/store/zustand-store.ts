29b80c45edfa14320ffb670621cc1033cd9f27cf
Iván Bautista <ivanbautistag@gmail.com>
2024-08-03 19:12:57 -0600

    feat: Add Zustand store for local state management

--- src/store/zustand-store.ts
+++ src/store/zustand-store.ts
@@ -1,13 +1,22 @@
 import { create } from 'zustand'
 import { produce } from 'immer'
+import { ButtonProps } from '@/components/ui/button'
 
-
-
+type OpenAiType = {
+  key: string | null
+  model: string
+}
 interface State {
-
+  openAiKey: OpenAiType
 }
 interface Actions {
+  setOpenAiKey: (openAI: OpenAiType) => void
 }
 
 export const useAppStore = create<State & Actions>((set) => ({
+  openAiKey: { key: null, model: '' },
+  setOpenAiKey: (openAI: OpenAiType) =>
+    set((prevState) => ({
+      openAiKey: { ...prevState.openAiKey, ...openAI },
+    })),
 }))
