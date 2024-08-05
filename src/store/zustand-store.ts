2cd1a192726ee80f1a8df5ed1f08bf47444597c9
Iván Bautista <ivanbautistag@gmail.com>
2024-08-05 16:59:24 -0600

    feat: store setup data and main menu open to zustand

--- src/store/zustand-store.ts
+++ src/store/zustand-store.ts
@@ -1,22 +1,44 @@
 import { create } from 'zustand'
-import { produce } from 'immer'
-import { ButtonProps } from '@/components/ui/button'
 
 type OpenAiType = {
   key: string | null
+}
+type chatSetup = {
   model: string
+  topic: string
+  level: string
+  role: string
 }
 interface State {
+  openMenu: boolean
   openAiKey: OpenAiType
+  chatSetup: chatSetup
 }
 interface Actions {
   setOpenAiKey: (openAI: OpenAiType) => void
+  setChatSetup: (chatSetup: chatSetup) => void
+  setOpenMenu: (openMenu: boolean) => void
 }
 
 export const useAppStore = create<State & Actions>((set) => ({
-  openAiKey: { key: null, model: '' },
+  openMenu: false,
+  openAiKey: { key: null },
+  chatSetup: {
+    model: '',
+    topic: '',
+    level: '',
+    role: '',
+  },
   setOpenAiKey: (openAI: OpenAiType) =>
     set((prevState) => ({
       openAiKey: { ...prevState.openAiKey, ...openAI },
     })),
+  setChatSetup: (chatSetup: chatSetup) =>
+    set((prevState) => ({
+      chatSetup: { ...prevState.chatSetup, ...chatSetup },
+    })),
+  setOpenMenu: (openMenu: boolean) =>
+    set({
+      openMenu: openMenu,
+    }),
 }))
