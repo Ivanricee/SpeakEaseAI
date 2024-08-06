16b1ffad901eafe1d755ae1853114da15ce861ed
Iván Bautista <ivanbautistag@gmail.com>
2024-08-06 11:49:31 -0600

    feat: add api key to form an return de text

--- src/components/hooks/useSpeechToText.ts
+++ src/components/hooks/useSpeechToText.ts
@@ -1,15 +1,21 @@
 import { speechToText } from '@/app/actions/openai'
+import { useAppStore } from '@/store/zustand-store'
 
 type hookReturnType = {
-  transcribeAudio: (audioBlob: Blob) => void
+  transcribeAudio: (audioBlob: Blob) => Promise<string>
 }
 export const useSpeechToText = (): hookReturnType => {
-  const transcribeAudio = (audioBlob: Blob) => {
+  const { openAiKey } = useAppStore((state) => ({
+    openAiKey: state.openAiKey.key,
+  }))
+  const transcribeAudio = async (audioBlob: Blob) => {
+    if (!openAiKey) return 'api key not set'
+
     const formData = new FormData()
     formData.append('audio', audioBlob, 'recording.webm')
-    speechToText({ formData })
-
-    return 'text from audio here: '
+    formData.append('apiKey', openAiKey)
+    const { text } = await speechToText({ formData })
+    return text
   }
   return { transcribeAudio }
 }
