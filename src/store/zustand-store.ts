1e932c67068fe0fd1335f9382c42ec4e30ce1f35
Iván Bautista <ivanbautistag@gmail.com>
2024-08-06 11:50:44 -0600

    feat: add conversation history to zustand store

--- src/store/zustand-store.ts
+++ src/store/zustand-store.ts
@@ -1,3 +1,4 @@
+import { CoreMessage } from 'ai'
 import { create } from 'zustand'
 
 type OpenAiType = {
@@ -9,15 +10,29 @@ type chatSetup = {
   level: string
   role: string
 }
+
+export type ExtendCoreMessage = {
+  id: string
+  role: 'user' | 'assistant' | 'system' | 'tool' | 'function'
+  content: string
+}
 interface State {
+  conversation: ExtendCoreMessage[] | []
   openMenu: boolean
   openAiKey: OpenAiType
   chatSetup: chatSetup
+}
+type setConversation = {
+  messages: ExtendCoreMessage[]
+  textContent: string
+  id: string
 }
 interface Actions {
   setOpenAiKey: (openAI: OpenAiType) => void
   setChatSetup: (chatSetup: chatSetup) => void
   setOpenMenu: (openMenu: boolean) => void
+  setConversation: ({ messages, textContent, id }: setConversation) => void
+  initConversation: (message: ExtendCoreMessage[]) => void
 }
 
 export const useAppStore = create<State & Actions>((set) => ({
@@ -29,6 +44,7 @@ export const useAppStore = create<State & Actions>((set) => ({
     level: '',
     role: '',
   },
+  conversation: [],
   setOpenAiKey: (openAI: OpenAiType) =>
     set((prevState) => ({
       openAiKey: { ...prevState.openAiKey, ...openAI },
@@ -41,4 +57,12 @@ export const useAppStore = create<State & Actions>((set) => ({
     set({
       openMenu: openMenu,
     }),
+  setConversation: ({ messages, textContent, id }: setConversation) =>
+    set({
+      conversation: [...messages, { id, role: 'assistant', content: textContent }],
+    }),
+  initConversation: (message: ExtendCoreMessage[]) =>
+    set((prevState) => ({
+      conversation: [...message],
+    })),
 }))
