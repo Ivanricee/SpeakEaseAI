20d82984fa532b262a9268a5a27e241b752d1261
Iván Bautista <ivanbautistag@gmail.com>
2024-08-11 18:34:29 -0600

    refactor: Adjust separator to avoid unnecessary width usage

--- src/app/chat-ai/page.tsx
+++ src/app/chat-ai/page.tsx
@@ -1,27 +1,23 @@
 /* eslint-disable max-len */
 
-'use client'
 import Chat from '@/components/Chat'
 import { Menu } from '@/components/Menu'
 import Microphone from '@/components/Microphone'
-import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
-import { useChat } from 'ai/react'
 
 export default function Setup() {
-  //const { messages, input, handleInputChange, handleSubmit } = useChat()
   return (
-    <main className="min-w-dvw font-robotoSlab container mx-auto h-dvh w-full max-w-screen-xl bg-background">
+    <main className="min-w-dvw container mx-auto h-dvh w-full max-w-screen-xl bg-background font-robotoSlab">
       <div
         className={
-          'flex h-full flex-col-reverse bg-yellow-800/25 md:flex md:h-full md:w-full md:flex-row md:flex-nowrap'
+          'flex h-full flex-col-reverse md:flex md:h-full md:w-full md:flex-row md:flex-nowrap'
         }
       >
-        <section className="h-2/6 bg-red-400/10 md:h-full md:w-4/12">
+        <section className="relative h-2/6 md:h-full md:w-4/12">
           <Microphone />
-          <Separator />
+          <Separator className="absolute right-0 top-0 grid" orientation="vertical" />
         </section>
-        <section className="h-4/6 bg-green-600/15 md:h-full md:w-8/12">
+        <section className="h-4/6 md:h-full md:w-8/12">
           <Chat />
           <Menu />
         </section>
