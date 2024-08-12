f5abbcb2f93f2ab5972abb229f707fdb7cc5dd35
Iván Bautista <ivanbautistag@gmail.com>
2024-08-11 18:32:32 -0600

    fet: add timer

--- src/components/Microphone.tsx
+++ src/components/Microphone.tsx
@@ -1,24 +1,45 @@
 'use client'
 
+import { useAppStore } from '@/store/zustand-store'
 import { useAudioRecorder } from './hooks/useAudioRecorder'
 import { Button } from './ui/button'
-import useAiChat from './hooks/useAIChat'
-import { IconMicrophone, IconMicrophoneOff } from '@tabler/icons-react'
-import { useTextToSpeech } from './hooks/useTextToSpeech'
+import CircularProgress from './ui/circularProgress'
+import { useShallow } from 'zustand/react/shallow'
 
 export default function Microphone() {
+  const { disableMicro } = useAppStore(
+    useShallow((state) => ({
+      disableMicro: state.disableMicro,
+    }))
+  )
   const { audioUrl, fullAudioUrl, isRecording, startRecording, stopRecording, getSpeechAssesment } =
     useAudioRecorder()
+
   const recordingAction = () => {
     if (!isRecording) return startRecording()
     return stopRecording()
   }
+
+  const shapeBtn = isRecording ? 'h-24 w-24 rounded-3xl' : 'h-32 w-32 rounded-full'
   return (
-    <section>
-      <section className="flex flex-col items-center justify-center gap-5">
-        <Button onClick={recordingAction}>
-          {isRecording ? <IconMicrophoneOff /> : <IconMicrophone />}
-        </Button>
+    <section className="flex h-full flex-col items-center justify-center">
+      <div className="relative h-full w-full gap-5">
+        <div className="absolute inset-0 flex h-full w-full items-center justify-center">
+          <Button
+            onClick={recordingAction}
+            disabled={disableMicro}
+            className={`z-10 aspect-square ${shapeBtn} transition-all duration-500 ease-in-out`}
+          />
+        </div>
+
+        <CircularProgress
+          className="absolute inset-0"
+          startTimer={isRecording}
+          onResetTimer={stopRecording}
+        />
+      </div>
+
+      <div className="hidden">
         <Button onClick={getSpeechAssesment}>Get Full Recording</Button>
         {isRecording && <h2 className="bg-red-500 p-4 text-white">Recording...</h2>}
 
@@ -31,7 +52,7 @@ export default function Microphone() {
             </audio>
           </div>
         )}
-      </section>
+      </div>
     </section>
   )
 }
