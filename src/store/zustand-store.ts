ba91bde5ca1f496166ccd6c22136bb7d924413cd
Iván Bautista <ivanbautistag@gmail.com>
2024-08-11 18:26:04 -0600

    feat: add  variable and function to enable recording button

--- src/store/zustand-store.ts
+++ src/store/zustand-store.ts
@@ -1,4 +1,3 @@
-import { CoreMessage } from 'ai'
 import { create } from 'zustand'
 
 type OpenAiType = {
@@ -15,27 +14,32 @@ export type ExtendCoreMessage = {
   id: string
   role: 'user' | 'assistant' | 'system' | 'tool' | 'function'
   content: string
-}
-interface State {
-  conversation: ExtendCoreMessage[] | []
-  openMenu: boolean
-  openAiKey: OpenAiType
-  chatSetup: chatSetup
+  url: string
 }
 type setConversation = {
   messages: ExtendCoreMessage[]
   textContent: string
   id: string
+  url: string
+}
+interface State {
+  disableMicro: boolean
+  chatSetup: chatSetup
+  conversation: ExtendCoreMessage[] | []
+  openMenu: boolean
+  openAiKey: OpenAiType
 }
 interface Actions {
-  setOpenAiKey: (openAI: OpenAiType) => void
-  setChatSetup: (chatSetup: chatSetup) => void
-  setOpenMenu: (openMenu: boolean) => void
-  setConversation: ({ messages, textContent, id }: setConversation) => void
+  setDisableMicro: (enableMicro: boolean) => void
   initConversation: (message: ExtendCoreMessage[]) => void
+  setChatSetup: (chatSetup: chatSetup) => void
+  setConversation: ({ messages, textContent, id }: setConversation) => void
+  setOpenAiKey: (openAI: OpenAiType) => void
+  setOpenMenu: (openMenu: boolean) => void
 }
 
 export const useAppStore = create<State & Actions>((set) => ({
+  disableMicro: false,
   openMenu: false,
   openAiKey: { key: null },
   chatSetup: {
@@ -45,6 +49,7 @@ export const useAppStore = create<State & Actions>((set) => ({
     role: '',
   },
   conversation: [],
+  setDisableMicro: (disableMicro: boolean) => set({ disableMicro }),
   setOpenAiKey: (openAI: OpenAiType) =>
     set((prevState) => ({
       openAiKey: { ...prevState.openAiKey, ...openAI },
@@ -57,9 +62,9 @@ export const useAppStore = create<State & Actions>((set) => ({
     set({
       openMenu: openMenu,
     }),
-  setConversation: ({ messages, textContent, id }: setConversation) =>
+  setConversation: ({ messages, textContent, id, url }: setConversation) =>
     set({
-      conversation: [...messages, { id, role: 'assistant', content: textContent }],
+      conversation: [...messages, { id, role: 'assistant', content: textContent, url }],
     }),
   initConversation: (message: ExtendCoreMessage[]) =>
     set((prevState) => ({
