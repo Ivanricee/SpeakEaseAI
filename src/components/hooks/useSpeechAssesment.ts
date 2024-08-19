01fc566ea82e8fb00b19541be1544979666ffd34
Iván Bautista <ivanbautistag@gmail.com>
2024-08-19 14:22:02 -0600

    fix: Convert webm blob file to WAV format for Azure; send to action server

--- src/components/hooks/useSpeechAssesment.ts
+++ src/components/hooks/useSpeechAssesment.ts
@@ -1,15 +1,33 @@
 import { speechToTextAssesment } from '@/app/actions/azure'
+import { useAppStore } from '@/store/zustand-store'
+import { AssessmentResult } from '@/types/assesmentResult'
+import Crunker from 'crunker'
 
 type hookReturnType = {
-  speechAssesment: (audioBlob: Blob) => void
+  speechAssesment: ({ audioBlob, referenceText, id }: AssesmenType) => void
 }
-export const useSpeechAssesment = (): hookReturnType => {
-  const speechAssesment = (audioBlob: Blob) => {
-    const formData = new FormData()
-    formData.append('audio', audioBlob, 'recordCombined.wav')
-    speechToTextAssesment({ formData })
+type AssesmenType = {
+  audioBlob: Blob
+  referenceText: string
+  id: string
+}
 
-    return 'result text from audio assesmenthere: '
+export const useSpeechAssesment = (): hookReturnType => {
+  const { addUserAssessment } = useAppStore((state) => ({
+    addUserAssessment: state.addUserAssessment,
+  }))
+  const speechAssesment = async ({ audioBlob, referenceText, id }: AssesmenType) => {
+    let crunker = new Crunker({ sampleRate: 16000 })
+    const audioBuffer = await crunker.fetchAudio(audioBlob)
+    const audioWav = await crunker.export(audioBuffer[0], 'audio/wav')
+    const formData = new FormData()
+    formData.append('audio', audioWav.blob, 'userAssesment.wav')
+    formData.append('referenceText', referenceText)
+    const resAssesmentStr = await speechToTextAssesment({ formData })
+    const resAssesment = JSON.parse(resAssesmentStr)
+    resAssesment.privPronJson.id = id
+    addUserAssessment({ id, userAssessment: resAssesment.privPronJson })
+    //return resAssesment.privPronJson as AssessmentResult
   }
   return { speechAssesment }
 }
