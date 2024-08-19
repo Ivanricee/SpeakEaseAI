081c6be023a0b4ab7ece5cec19357b2d9010ff3f
Iván Bautista <ivanbautistag@gmail.com>
2024-08-19 13:51:02 -0600

    feat: Add referenceText and return JSON result as string and Promise for success/error handling

--- src/app/actions/azure.ts
+++ src/app/actions/azure.ts
@@ -22,20 +22,15 @@ const speechConfig = SpeechConfig.fromSubscription(
 )
 speechConfig.speechRecognitionLanguage = 'en-US'
 speechConfig.setProperty(PropertyId.Speech_SegmentationSilenceTimeoutMs, '5000')
-interface SpeechToTextAssesment {
+interface UserAssesment {
   formData: FormData
 }
 
-export async function speechToTextAssesment({
-  formData,
-}: SpeechToTextAssesment) {
+export async function speechToTextAssesment({ formData }: UserAssesment): Promise<string> {
   'use server'
   const audio = formData.get('audio') as File
-  //const apiKey = formData.get('apiKey')
-  if (!audio) return
-  console.log('formdata ', {
-    data: formData.get('audio'),
-  })
+  const referenceText = formData.get('referenceText') as string
+  if (!audio || !referenceText) return JSON.stringify({ error: 'Audio o referencia not found' })
 
   const buffer = await audio.arrayBuffer()
   const audioBuffer = Buffer.from(buffer)
@@ -43,62 +38,67 @@ export async function speechToTextAssesment({
   try {
     const auioPath = `temp/${audio.name}`
     await fs.writeFileSync(auioPath, uint8Array)
-    await getSpeechAssesment(auioPath)
+    const resAssesment = await getSpeechAssesment(auioPath)
+
     fs.unlinkSync(auioPath)
-    //return NextResponse.json(transcription)
+    return JSON.stringify(resAssesment)
   } catch (error) {
     console.error('Failed to write or delete file:', error)
+    return JSON.stringify({ error: 'Failed in file or get assesment' })
   }
 
   async function getSpeechAssesment(filePath: string) {
     const audio = fs.readFileSync(filePath)
     const audioConfig = AudioConfig.fromWavFileInput(audio)
-    console.log({ filePath, audio, audioConfig })
-
+    //let resAssesmentResult: any = null
     //Prnunciation assesment
     const pronAssesmentConf = new PronunciationAssessmentConfig(
-      '',
+      referenceText,
       PronunciationAssessmentGradingSystem.HundredMark,
       PronunciationAssessmentGranularity.Phoneme,
       false
     )
     pronAssesmentConf.enableProsodyAssessment = true
-    pronAssesmentConf.enableContentAssessmentWithTopic(
-      'establecer aqui topic hablado'
-    )
+    //pronAssesmentConf.enableContentAssessmentWithTopic('establecer aqui topic hablado')
     //---------------------
     //speech recognizer
     let speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig)
     pronAssesmentConf.applyTo(speechRecognizer)
 
-    speechRecognizer.recognizeOnceAsync((result) => {
-      switch (result.reason) {
-        case ResultReason.RecognizedSpeech:
-          onRecognizedResult(result)
-          break
-        case ResultReason.NoMatch:
-          console.log('NOMATCH: Speech could not be recognized.')
-          break
-        case ResultReason.Canceled:
-          const cancellation = CancellationDetails.fromResult(result)
-          console.log(`CANCELED: Reason=${cancellation.reason}`)
+    return new Promise((resolve, reject) => {
+      speechRecognizer.recognizeOnceAsync(
+        (result) => {
+          switch (result.reason) {
+            case ResultReason.RecognizedSpeech:
+              const pronunciation_result = PronunciationAssessmentResult.fromResult(result)
+              resolve(pronunciation_result)
+              break
+            case ResultReason.NoMatch:
+              resolve({ error: 'NOMATCH: Speech could not be recognized.' })
+              break
+            case ResultReason.Canceled:
+              const cancellation = CancellationDetails.fromResult(result)
+              console.log(`CANCELED: Reason=${cancellation.reason}`)
 
-          if (cancellation.reason == CancellationReason.Error) {
-            console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`)
-            console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`)
-            console.log(
-              'CANCELED: Did you set the speech resource key and region values?'
-            )
+              if (cancellation.reason == CancellationReason.Error) {
+                console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`)
+                console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`)
+                resolve({
+                  error: 'CANCELED: Did you set the speech resource key and region values?',
+                })
+              }
+              break
           }
-          break
-      }
-      speechRecognizer.close()
+          speechRecognizer.close()
+        },
+        (err) => {
+          console.error(err)
+          reject(err) // Rechaza la promesa con el error
+        }
+      )
     })
-    function onRecognizedResult(result: RecognitionResult) {
+    /* type RecognitionResult
       console.log('pronunciation assessment for: ', result.text)
-      const pronunciation_result =
-        PronunciationAssessmentResult.fromResult(result)
-      console.log('RESULTADO: ', pronunciation_result)
 
       console.log(
         ' Accuracy score: ',
@@ -130,7 +130,6 @@ export async function speechToTextAssesment({
         console.log(`    ${idx + 1}: word: ${word.Word} \n
           acuracy score: ${word.PronunciationAssessment?.AccuracyScore} \n
           error type: ${word.PronunciationAssessment?.ErrorType}`)
-      })
-    }
+      })*/
   }
 }
