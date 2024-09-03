ec6e0f5dd48fb45cb9672aaadd08114e0cdabd5a
Iván Bautista <ivanbautistag@gmail.com>
2024-09-03 13:53:00 -0600

    feat: Add key and region from form for Azure assessment; use audio buffer and IPA phonemes

--- src/app/actions/azure.ts
+++ src/app/actions/azure.ts
@@ -1,6 +1,5 @@
 'use server'
 
-import fs from 'fs'
 import {
   AudioConfig,
   SpeechConfig,
@@ -13,15 +12,8 @@ import {
   PronunciationAssessmentGradingSystem,
   PronunciationAssessmentGranularity,
   PronunciationAssessmentResult,
-  RecognitionResult,
 } from 'microsoft-cognitiveservices-speech-sdk'
 
-const speechConfig = SpeechConfig.fromSubscription(
-  process.env.AZ_SPEECH_KEY as string,
-  process.env.AZ_SPEECH_REGION as string
-)
-speechConfig.speechRecognitionLanguage = 'en-US'
-speechConfig.setProperty(PropertyId.Speech_SegmentationSilenceTimeoutMs, '5000')
 interface UserAssesment {
   formData: FormData
 }
@@ -30,27 +22,30 @@ export async function speechToTextAssesment({ formData }: UserAssesment): Promis
   'use server'
   const audio = formData.get('audio') as File
   const referenceText = formData.get('referenceText') as string
-  if (!audio || !referenceText) return JSON.stringify({ error: 'Audio o referencia not found' })
+  const azureKey = formData.get('key') as string
+  const azureRegion = formData.get('region') as string
 
-  const buffer = await audio.arrayBuffer()
-  const audioBuffer = Buffer.from(buffer)
-  const uint8Array = new Uint8Array(audioBuffer)
+  if (!audio || !referenceText || !azureKey)
+    return JSON.stringify({ error: 'Audio o referencia not found' })
+
   try {
-    const auioPath = `temp/${audio.name}`
-    await fs.writeFileSync(auioPath, uint8Array)
-    const resAssesment = await getSpeechAssesment(auioPath)
-
-    fs.unlinkSync(auioPath)
+    const resAssesment = await getSpeechAssesment(audio, azureKey, azureRegion)
     return JSON.stringify(resAssesment)
   } catch (error) {
     console.error('Failed to write or delete file:', error)
     return JSON.stringify({ error: 'Failed in file or get assesment' })
   }
 
-  async function getSpeechAssesment(filePath: string) {
-    const audio = fs.readFileSync(filePath)
-    const audioConfig = AudioConfig.fromWavFileInput(audio)
-    //let resAssesmentResult: any = null
+  async function getSpeechAssesment(audioFile: File, azureKey: string, azureRegion: string) {
+    const speechConfig = SpeechConfig.fromSubscription(azureKey, azureRegion)
+    speechConfig.speechRecognitionLanguage = 'en-US'
+    speechConfig.setProperty(PropertyId.Speech_SegmentationSilenceTimeoutMs, '5000')
+
+    const buffer = await audioFile.arrayBuffer()
+    const audioBuffer = Buffer.from(buffer)
+    const audioConfig = AudioConfig.fromWavFileInput(audioBuffer)
+    AudioConfig.fromWavFileInput
+
     //Prnunciation assesment
     const pronAssesmentConf = new PronunciationAssessmentConfig(
       referenceText,
@@ -58,6 +53,7 @@ export async function speechToTextAssesment({ formData }: UserAssesment): Promis
       PronunciationAssessmentGranularity.Phoneme,
       false
     )
+    pronAssesmentConf.phonemeAlphabet = 'IPA'
     pronAssesmentConf.enableProsodyAssessment = true
     //pronAssesmentConf.enableContentAssessmentWithTopic('establecer aqui topic hablado')
     //---------------------
@@ -68,6 +64,7 @@ export async function speechToTextAssesment({ formData }: UserAssesment): Promis
     return new Promise((resolve, reject) => {
       speechRecognizer.recognizeOnceAsync(
         (result) => {
+          //console.log({ key: pronAssesmentConf.properties, result })
           switch (result.reason) {
             case ResultReason.RecognizedSpeech:
               const pronunciation_result = PronunciationAssessmentResult.fromResult(result)
@@ -84,7 +81,7 @@ export async function speechToTextAssesment({ formData }: UserAssesment): Promis
                 console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`)
                 console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`)
                 resolve({
-                  error: 'CANCELED: Did you set the speech resource key and region values?',
+                  error: 'Error in speech, resource, key and region not set',
                 })
               }
               break
@@ -99,37 +96,29 @@ export async function speechToTextAssesment({ formData }: UserAssesment): Promis
     })
     /* type RecognitionResult
       console.log('pronunciation assessment for: ', result.text)
-
-      console.log(
-        ' Accuracy score: ',
-        pronunciation_result.accuracyScore,
-        '\n',
-        'pronunciation score: ',
-        pronunciation_result.pronunciationScore,
-        '\n',
-        'completeness score : ',
-        pronunciation_result.completenessScore,
-        '\n',
-        'fluency score: ',
-        pronunciation_result.fluencyScore,
-        '\n',
-        'prosody score: ',
-        pronunciation_result.prosodyScore,
-        '\n',
         'grammar: ',
         pronunciation_result.contentAssessmentResult.grammarScore,
         'vocabulary: ',
         pronunciation_result.contentAssessmentResult.vocabularyScore,
         'topic score: ',
         pronunciation_result.contentAssessmentResult.topicScore
-      )
-      console.log('  Word-level details:')
-      pronunciation_result.detailResult.Words.forEach((word, idx) => {
-        //console.log({ word })
+    */
+  }
+}
 
-        console.log(`    ${idx + 1}: word: ${word.Word} \n
-          acuracy score: ${word.PronunciationAssessment?.AccuracyScore} \n
-          error type: ${word.PronunciationAssessment?.ErrorType}`)
-      })*/
+export const validateAzureKey = async (azureKey: string, azureRegion: string) => {
+  const endpoint = `https://${azureRegion}.api.cognitive.microsoft.com/sts/v1.0/issueToken`
+  try {
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Ocp-Apim-Subscription-Key': azureKey,
+      },
+    })
+    return response.ok
+  } catch (e: any) {
+    console.log('Azue Authentication error', e.message)
+    return false
   }
 }
