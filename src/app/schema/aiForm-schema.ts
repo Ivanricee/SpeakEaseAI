dc8db938b1ddd22db21e0f64f63e6f246d27616e
Iván Bautista <ivanbautistag@gmail.com>
2024-09-03 13:58:03 -0600

    Feat: Add validation schema for optional Azure key, activated only when checkbox is enabled

--- src/app/schema/aiForm-schema.ts
+++ src/app/schema/aiForm-schema.ts
@@ -1,22 +1,82 @@
 import { z } from 'zod'
 
-export const AIFormSchema = z.object({
-  key: z
-    .string({
-      required_error: 'Enter your OpenAI API key',
-    })
-    .min(10, {
-      message: 'Api must not be empty',
-    })
-    .max(200)
-    .refine(
-      (value) => {
-        if (!/\s/.test(value)) return true // just spaces
-        if (/^\s+|\s+$/.test(value)) return false //start/end with spaces
+export const AIFormSchema = z
+  .object({
+    key: z
+      .string({
+        required_error: 'Enter your OpenAI API key',
+      })
+      .min(10, {
+        message: 'Api must not be empty',
+      })
+      .max(200)
+      .refine(
+        (value) => {
+          if (!/\s/.test(value)) return true // just spaces
+          if (/^\s+|\s+$/.test(value)) return false //start/end with spaces
+          return true
+        },
+        {
+          message: 'The APi key must not be empty, spaces only, or start/end with spaces.',
+        }
+      ),
+    enableAzure: z.boolean().default(false),
+    azureKey: z
+      .string({
+        required_error: 'Enter your Speech Resource Key',
+      })
+      .refine(
+        (value) => {
+          if (!/\s/.test(value)) return true // just spaces
+          if (/^\s+|\s+$/.test(value)) return false //start/end with spaces
+          return true
+        },
+        {
+          message: 'The APi key must not be empty, spaces only, or start/end with spaces.',
+        }
+      )
+      .optional(),
+    azureRegion: z
+      .string({
+        required_error: 'Enter your Speech Resource Key',
+      })
+      .refine(
+        (value) => {
+          if (!/\s/.test(value)) return true // just spaces
+          if (/^\s+|\s+$/.test(value)) return false //start/end with spaces
+          return true
+        },
+        {
+          message: 'The Region/Location must not be empty, spaces only, or start/end with spaces.',
+        }
+      )
+      .optional(),
+  })
+  .refine(
+    (data) => {
+      if (!data.enableAzure) return true
+      if (!data.azureRegion || data.azureRegion.length < 4) return false
+      if (data.azureRegion && data.azureRegion.length >= 4 && data.azureRegion.length <= 30) {
         return true
-      },
-      {
-        message: 'The APi key must not be empty, spaces only, or start/end with spaces.',
       }
-    ),
-})
+      return false
+    },
+    {
+      message: 'Length must be between 4 and 30',
+      path: ['azureRegion'],
+    }
+  )
+  .refine(
+    (data) => {
+      if (!data.enableAzure) return true
+      if (!data.azureKey || data.azureKey.length < 10) return false
+      if (data.azureKey && data.azureKey.length >= 10 && data.azureKey.length <= 50) {
+        return true
+      }
+      return false
+    },
+    {
+      message: 'Length must be between 10 and 50',
+      path: ['azureKey'],
+    }
+  )
--- src/components/AIForm.tsx
+++ src/components/AIForm.tsx
@@ -1,73 +1,68 @@
 'use client'
 
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card'
 import { AIFormSchema } from '@/app/schema/aiForm-schema'
 import { z } from 'zod'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 import { useFormState } from 'react-dom'
 import { KeyState, setKey } from '@/app/actions/openai'
-import { useEffect, useRef, useState } from 'react'
-import { useAppStore } from '@/store/zustand-store'
-import { Alert, AlertDescription, AlertTitle } from './ui/alert'
-import { IconCheckbox } from '@tabler/icons-react'
-interface CurrentOpenAi {
-  key: string
-}
+import { Alert, AlertTitle } from './ui/alert'
+import { IconCheckbox, IconInfoCircleFilled, IconX } from '@tabler/icons-react'
+import Link from 'next/link'
+import { Checkbox } from './ui/checkbox'
+import useAiFormSetup from './hooks/useAiFormSetup'
+import { useEffect } from 'react'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+import { Separator } from './ui/separator'
+
 export default function AIForm() {
-  const [status, setStatus] = useState<boolean>(false)
-  const currentOpenAi = useRef<CurrentOpenAi>({ key: '' })
-  const { openAiKey, setOpenAiKey } = useAppStore((state) => ({
-    openAiKey: state.openAiKey,
-    setOpenAiKey: state.setOpenAiKey,
-  }))
-  const defaultValues = {
-    key: '',
-  }
-  const form = useForm<z.infer<typeof AIFormSchema>>({
-    resolver: zodResolver(AIFormSchema),
-    defaultValues,
-  })
   const [state, formAction] = useFormState<KeyState, FormData>(setKey, {
-    isValidKey: null,
+    isOpenAiValidKey: null,
+    isAzureValidKey: null,
   })
+  const { form, setIsValidating, isValidating, validOpenAi, validAzure } = useAiFormSetup(state)
+  const isAzureEnabled = form.watch('enableAzure')
+
   useEffect(() => {
-    if (state?.isValidKey !== null && status) {
-      setStatus(false)
-    }
-    if (state.isValidKey) {
-      setOpenAiKey({
-        key: currentOpenAi.current.key,
-      })
-    } else if (state.isValidKey === false) {
-      setOpenAiKey(defaultValues)
-    }
+    const azureKey = form.getValues('azureKey')
+    const azureRegion = form.getValues('azureRegion')
+    if (!isAzureEnabled && azureKey && azureKey?.length > 0) form.setValue('azureKey', '')
+    if (!isAzureEnabled && azureRegion && azureRegion?.length > 0) form.setValue('azureRegion', '')
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [state])
+  }, [isAzureEnabled])
 
   const onSubmitKey = async (data: z.infer<typeof AIFormSchema>) => {
-    setStatus(true)
-    currentOpenAi.current = {
-      key: data.key,
-    }
-
+    setIsValidating(true)
     const formdata = new FormData()
     formdata.append('key', data.key)
+    formdata.append('enableAzure', data.enableAzure.toString())
+    formdata.append('azureKey', data.azureKey || '')
+    formdata.append('azureRegion', data.azureRegion || '')
+
     await formAction(formdata)
-    form.reset(defaultValues)
   }
-  const hasOpenAiKey = Boolean(openAiKey.key && openAiKey.key?.length > 0)
-  const showAlert = Boolean(state.isValidKey || hasOpenAiKey)
+  const invalidOpenAi = state.isOpenAiValidKey === false
+  const invalidAzure = state.isAzureValidKey === false
+  const invalidApiKey = invalidOpenAi || invalidAzure
+  const hasApiKey = validOpenAi || validAzure
 
   return (
     <Card className="mt-6">
       <CardHeader>
-        <CardTitle>Add OpenAI API Key</CardTitle>
+        <CardTitle>Configure your API Keys</CardTitle>
         <CardDescription>
-          The API key will be used for voice recognition (Whisper) and text generation (ChatGPT).
+          Use OpenAI for text generation and Whisper, and Azure Speech Services for audio
+          assessment.
         </CardDescription>
       </CardHeader>
       <CardContent>
@@ -78,7 +73,7 @@ export default function AIForm() {
               name="key"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Api Key</FormLabel>
+                  <FormLabel>OpenAi Key</FormLabel>
                   <FormControl>
                     <Input
                       placeholder="sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
@@ -91,29 +86,117 @@ export default function AIForm() {
                 </FormItem>
               )}
             />
-
-            <Button type="submit" disabled={status}>
-              {status ? 'validating...' : 'Use Key'}
+            <FormField
+              control={form.control}
+              name="enableAzure"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow">
+                  <FormControl>
+                    <Checkbox checked={field.value} onCheckedChange={field.onChange} />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormLabel>Enable Speech Assessment</FormLabel>
+                    <FormDescription className="mb-12">
+                      This feature requires a valid{' '}
+                      <Link
+                        target="_blank"
+                        rel="noreferrer"
+                        href={'https://azure.microsoft.com/free/cognitive-services'}
+                        className="text-primary"
+                      >
+                        Azure subscription.
+                      </Link>
+                    </FormDescription>
+                    {isAzureEnabled && (
+                      <div className="flex flex-col gap-4 pt-6">
+                        <FormField
+                          control={form.control}
+                          name="azureKey"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="flex items-center gap-1">
+                                Speech Resource Key
+                                <Popover>
+                                  <PopoverTrigger asChild>
+                                    <Button variant="outline" className="m-0 flex h-auto p-0">
+                                      <IconInfoCircleFilled size={22} className="cursor-pointer" />
+                                    </Button>
+                                  </PopoverTrigger>
+                                  <PopoverContent className="">
+                                    <div>
+                                      <h5 className="text-balance text-base font-normal leading-none">
+                                        <Link
+                                          target="_blank"
+                                          rel="noreferrer"
+                                          href={
+                                            'https://portal.azure.com/#create/Microsoft.CognitiveServicesSpeechServices'
+                                          }
+                                          className="text-primary"
+                                        >
+                                          Create a Speech resource
+                                        </Link>
+                                      </h5>
+                                    </div>
+                                  </PopoverContent>
+                                </Popover>
+                              </FormLabel>
+                              <FormControl>
+                                <Input
+                                  placeholder="xxxxxxxxxxxxxxxxxxxxxxx"
+                                  {...field}
+                                  type="password"
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                        <FormField
+                          control={form.control}
+                          name="azureRegion"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>Location/Region</FormLabel>
+                              <FormControl>
+                                <Input placeholder="eastus" {...field} type="text" />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </div>
+                    )}
+                  </div>
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              disabled={isValidating || !form.formState.isValid || form.formState.isValidating}
+            >
+              {isValidating ? 'validating...' : 'Validate'}
             </Button>
           </form>
         </Form>
         <section>
-          {showAlert && (
-            <Alert variant="success" className="mt-8">
+          {hasApiKey && (
+            <Alert
+              variant="success"
+              className="mt-4 flex flex-col justify-center bg-green-400/10 text-sm"
+            >
               <IconCheckbox className="text-green-500" />
-              <AlertTitle>Success!</AlertTitle>
-              <AlertDescription className="text-xs">
-                Your OpenAI API key has been validated successfully.
-              </AlertDescription>
+              {validOpenAi && <AlertTitle>Openai Key Validated</AlertTitle>}
+              {validAzure && <AlertTitle>Azure Key Validated</AlertTitle>}
             </Alert>
           )}
-          {state.isValidKey === false && (
-            <Alert variant="destructive" className="mt-8">
-              <IconCheckbox className="text-red-500" />
-              <AlertTitle>Error!</AlertTitle>
-              <AlertDescription className="text-xs">
-                Your OpenAI API key is invalid.
-              </AlertDescription>
+          {invalidApiKey && (
+            <Alert
+              variant="destructive"
+              className="mt-4 flex flex-col justify-center bg-red-400/10 text-sm text-red-500/80"
+            >
+              <IconX className="text-red-400" />
+              {invalidOpenAi && <AlertTitle>Openai key could not be validated</AlertTitle>}
+              {invalidAzure && <AlertTitle>Azure Key could not be validated</AlertTitle>}
             </Alert>
           )}
         </section>
