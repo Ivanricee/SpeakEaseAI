0cc9e268e20fd0a37224db4df31c30eedd235b43
Iván Bautista <ivanbautistag@gmail.com>
2024-09-03 14:19:57 -0600

    feat: add key and region to formData

--- src/components/hooks/useSpeechAssesment.ts
+++ src/components/hooks/useSpeechAssesment.ts
@@ -1,6 +1,5 @@
 import { speechToTextAssesment } from '@/app/actions/azure'
 import { useAppStore } from '@/store/zustand-store'
-import { AssessmentResult } from '@/types/assesmentResult'
 import Crunker from 'crunker'
 
 type hookReturnType = {
@@ -13,8 +12,10 @@ type AssesmenType = {
 }
 
 export const useSpeechAssesment = (): hookReturnType => {
-  const { addUserAssessment } = useAppStore((state) => ({
+  const { addUserAssessment, azureKey, azureRegion } = useAppStore((state) => ({
     addUserAssessment: state.addUserAssessment,
+    azureKey: state.azureKey.key,
+    azureRegion: state.azureKey.region,
   }))
   const speechAssesment = async ({ audioBlob, referenceText, id }: AssesmenType) => {
     let crunker = new Crunker({ sampleRate: 16000 })
@@ -23,11 +24,14 @@ export const useSpeechAssesment = (): hookReturnType => {
     const formData = new FormData()
     formData.append('audio', audioWav.blob, 'userAssesment.wav')
     formData.append('referenceText', referenceText)
+    formData.append('key', azureKey || '')
+    formData.append('region', azureRegion || '')
     const resAssesmentStr = await speechToTextAssesment({ formData })
     const resAssesment = JSON.parse(resAssesmentStr)
+    if (resAssesment.error) return console.log('error in assesment', resAssesment.error)
+
     resAssesment.privPronJson.id = id
     addUserAssessment({ id, userAssessment: resAssesment.privPronJson })
-    //return resAssesment.privPronJson as AssessmentResult
   }
   return { speechAssesment }
 }
