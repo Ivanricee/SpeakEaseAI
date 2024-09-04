7ab3da18b61a78e6fed0f12d1257e5bad364e3ac
Iván Bautista <ivanbautistag@gmail.com>
2024-09-04 16:39:38 -0600

    feat: Add temperature chart for speech assessment scores

--- src/components/SpeechAssessmentResult.tsx
+++ src/components/SpeechAssessmentResult.tsx
@@ -10,6 +10,7 @@ import {
 } from '@tabler/icons-react'
 import { useShallow } from 'zustand/react/shallow'
 import { Skeleton } from './ui/skeleton'
+import { ScoreProgress } from './ui/scoreProgress'
 
 interface Props {
   idAssessment: string | null
@@ -27,40 +28,55 @@ export default function SpeechAssessmentResult({ idAssessment }: Props) {
         <>
           <Separator orientation="horizontal" className="inset-0 my-2 flex" />
 
-          <div className="flex w-full items-center justify-center gap-2">
-            <div className="flex flex-col rounded-md border-2 border-stone-900 p-1">
-              <p className="flex flex-nowrap items-center gap-0.5">
-                {userAssesmnt.PronunciationAssessment.AccuracyScore === 0 ? (
-                  <IconX />
-                ) : (
-                  <IconCheck />
-                )}
-                {userAssesmnt.PronunciationAssessment.AccuracyScore}%
+          <div className="flex w-full items-center justify-center gap-2 font-robotoSlab text-stone-900">
+            <div className="flex flex-col rounded-md border-2 border-stone-900 p-1 text-center">
+              <p className="flex flex-nowrap items-center gap-0.5 text-[0.8rem]">
+                {Math.floor(userAssesmnt.PronunciationAssessment.AccuracyScore)}%
+                <ScoreProgress score={userAssesmnt.PronunciationAssessment.AccuracyScore}>
+                  {userAssesmnt.PronunciationAssessment.AccuracyScore === 0 ? (
+                    <IconX size={15} className="text-stone-900" />
+                  ) : (
+                    <IconCheck size={15} className="text-stone-900" />
+                  )}
+                </ScoreProgress>
               </p>
               <h6 className="font-kadwa text-[0.7rem] font-bold">Accuracy </h6>
             </div>
+
             <div className="flex flex-col items-center rounded-md border-2 border-stone-900 p-1">
-              <p className="flex flex-nowrap items-center gap-0.5">
-                <IconPuzzle /> {userAssesmnt.PronunciationAssessment.CompletenessScore}%
+              <p className="flex flex-nowrap items-center gap-0.5 text-[0.8rem]">
+                {Math.floor(userAssesmnt.PronunciationAssessment.CompletenessScore)}%
+                <ScoreProgress score={userAssesmnt.PronunciationAssessment.CompletenessScore}>
+                  <IconPuzzle size={15} className="text-stone-900" />
+                </ScoreProgress>
               </p>
               <h6 className="font-kadwa text-[0.7rem] font-bold">Completeness </h6>
             </div>
 
             <div className="flex flex-col items-center rounded-md border-2 border-stone-900 p-1">
-              <p className="flex flex-nowrap items-center gap-0.5">
-                <IconTimeline /> {userAssesmnt.PronunciationAssessment.FluencyScore}%
+              <p className="flex flex-nowrap items-center gap-0.5 text-[0.8rem]">
+                {Math.floor(userAssesmnt.PronunciationAssessment.FluencyScore)}%
+                <ScoreProgress score={userAssesmnt.PronunciationAssessment.FluencyScore}>
+                  <IconTimeline size={15} className="text-stone-900" />
+                </ScoreProgress>
               </p>
               <h6 className="font-kadwa text-[0.7rem] font-bold">Fluency </h6>
             </div>
             <div className="flex flex-col items-center rounded-md border-2 border-stone-900 p-1">
-              <p className="flex flex-nowrap items-center gap-0.5">
-                <IconSpeakerphone /> {userAssesmnt.PronunciationAssessment.PronScore}%
+              <p className="flex flex-nowrap items-center gap-0.5 text-[0.8rem]">
+                {Math.floor(userAssesmnt.PronunciationAssessment.PronScore)}%
+                <ScoreProgress score={userAssesmnt.PronunciationAssessment.PronScore}>
+                  <IconSpeakerphone size={15} className="text-stone-900" />
+                </ScoreProgress>
               </p>
               <h6 className="font-kadwa text-[0.7rem] font-bold">Pronunciation </h6>
             </div>
             <div className="flex flex-col items-center rounded-md border-2 border-stone-900 p-1">
-              <p className="flex flex-nowrap items-center gap-0.5">
-                <IconMusic /> {userAssesmnt.PronunciationAssessment.ProsodyScore}%
+              <p className="flex flex-nowrap items-center gap-0.5 text-[0.8rem]">
+                {Math.floor(userAssesmnt.PronunciationAssessment.ProsodyScore)}%
+                <ScoreProgress score={userAssesmnt.PronunciationAssessment.ProsodyScore}>
+                  <IconMusic size={15} className="text-stone-900" />
+                </ScoreProgress>
               </p>
               <h6 className="font-kadwa text-[0.7rem] font-bold">Prosody </h6>
             </div>
@@ -79,39 +95,39 @@ export default function SpeechAssessmentResult({ idAssessment }: Props) {
 function ResultSkeleton() {
   return (
     <div className="flex w-full items-center justify-center gap-2">
-      <div className="flex flex-col rounded-md border-2 border-stone-900 p-1">
+      <div className="flex flex-col justify-center rounded-md border-2 border-stone-900 p-1">
         <p className="flex flex-nowrap items-center gap-0.5">
-          <Skeleton className="h-6 w-6" />
-          <Skeleton className="h-6 w-8" />
+          <Skeleton className="h-5 w-8" />
+          <Skeleton className="h-7 w-7 rounded-full" />
         </p>
         <h6 className="font-kadwa text-[0.7rem] font-bold">Accuracy </h6>
       </div>
       <div className="flex flex-col items-center rounded-md border-2 border-stone-900 p-1">
         <p className="flex flex-nowrap items-center gap-0.5">
-          <Skeleton className="h-6 w-6" />
-          <Skeleton className="h-6 w-8" />
+          <Skeleton className="h-5 w-6" />
+          <Skeleton className="h-7 w-7 rounded-full" />
         </p>
         <h6 className="font-kadwa text-[0.7rem] font-bold">Completeness </h6>
       </div>
 
       <div className="flex flex-col items-center rounded-md border-2 border-stone-900 p-1">
         <p className="flex flex-nowrap items-center gap-0.5">
-          <Skeleton className="h-6 w-6" />
-          <Skeleton className="h-6 w-7" />
+          <Skeleton className="h-5 w-8" />
+          <Skeleton className="h-7 w-7 rounded-full" />
         </p>
         <h6 className="font-kadwa text-[0.7rem] font-bold">Fluency </h6>
       </div>
       <div className="flex flex-col items-center rounded-md border-2 border-stone-900 p-1">
         <p className="flex flex-nowrap items-center gap-0.5">
-          <Skeleton className="h-6 w-6" />
-          <Skeleton className="h-6 w-8" />
+          <Skeleton className="h-5 w-6" />
+          <Skeleton className="h-7 w-7 rounded-full" />
         </p>
         <h6 className="font-kadwa text-[0.7rem] font-bold">Pronunciation </h6>
       </div>
-      <div className="flex flex-col items-center rounded-md border-2 border-stone-900 p-1">
+      <div className="flex flex-col items-center justify-center rounded-md border-2 border-stone-900 p-1">
         <p className="flex flex-nowrap items-center gap-0.5">
-          <Skeleton className="h-6 w-7" />
-          <Skeleton className="h-6 w-9" />
+          <Skeleton className="h-5 w-6" />
+          <Skeleton className="h-7 w-7 rounded-full" />
         </p>
         <h6 className="font-kadwa text-[0.7rem] font-bold">Prosody </h6>
       </div>
