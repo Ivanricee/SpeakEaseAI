2edf2b87c5ea39c43a2efb3b9cc4f889e0c44729
Iván Bautista <ivanbautistag@gmail.com>
2024-09-20 12:55:20 -0600

    feat: Add fade for typing effect

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,11 +78,16 @@ const config = {
           '0%': { transform: 'translateX(0)' },
           '100%': { transform: 'translateX(-8rem)' }, // Adjust as needed
         },
+        typingEffect: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'slide-out-to-left-m': 'accordion-down 0.2s ease-out',
+        typingFade: 'typingEffect 0.9s ease-out forwards',
       },
     },
   },
