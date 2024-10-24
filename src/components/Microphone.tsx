fc569a57cc4a8a1031d4aafec80aab204c3ea55e
Iván Bautista <ivanbautistag@gmail.com>
2024-10-24 17:24:47 -0600

    feat: add button to evaluate conversation

--- src/components/Microphone.tsx
+++ src/components/Microphone.tsx
@@ -5,7 +5,7 @@ import { useAudioRecorder } from './hooks/useAudioRecorder'
 import { Button } from './ui/button'
 import CircularProgress from './ui/circularProgress'
 import { useShallow } from 'zustand/react/shallow'
-import EvaluateConversation from './EvaluateConversation'
+import useEvaluateConversation from './hooks/useEvaluateConversation'
 
 export default function Microphone() {
   const { disableMicro } = useAppStore(
@@ -14,21 +14,32 @@ export default function Microphone() {
     }))
   )
   const { audioUrl, fullAudioUrl, isRecording, startRecording, stopRecording } = useAudioRecorder()
-
+  const { onEvaluate, responseCount } = useEvaluateConversation()
   const recordingAction = () => {
     if (!isRecording) return startRecording()
     return stopRecording()
   }
 
-  const shapeBtn = isRecording ? 'h-24 w-24 rounded-3xl' : 'h-32 w-32 rounded-full'
+  const shapeBtn = isRecording
+    ? 'lg:h-24 lg:w-24 h-16 w-16 rounded-3xl'
+    : 'h-20 w-20 lg:h-32 lg:w-32 rounded-full'
   return (
-    <section className="flex h-full flex-col items-center justify-center">
-      <div className="relative w-full gap-5">
+    <section className="flex h-full flex-col items-center justify-start lg:justify-center">
+      <div className="relative top-0 flex-col flex-wrap items-start justify-start gap-2 lg:top-48">
+        <Button
+          onClick={onEvaluate}
+          className="my-0 line-clamp-1 py-0"
+          disabled={responseCount < 5}
+        >
+          {responseCount}/5 responses to evaluate conversation
+        </Button>
+      </div>
+      <div className="relative top-14 w-full gap-5">
         <div className="absolute inset-0 flex h-full w-full items-center justify-center">
           <Button
             onClick={recordingAction}
             disabled={disableMicro}
-            className={`z-10 aspect-square ${shapeBtn} transition-all duration-500 ease-in-out`}
+            className={`z-10 aspect-square bg-teal-700/70 ${shapeBtn} transition-all duration-500 ease-in-out`}
           />
         </div>
 
@@ -37,9 +48,6 @@ export default function Microphone() {
           startTimer={isRecording}
           onResetTimer={stopRecording}
         />
-      </div>
-      <div className="relative top-24 flex flex-wrap items-center justify-center gap-2">
-        <EvaluateConversation />
       </div>
     </section>
   )
