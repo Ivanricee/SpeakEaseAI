cd5a87c3bdcddeb125d09aeefad16596e0a2eb75
Iván Bautista <ivanbautistag@gmail.com>
2024-10-24 16:52:49 -0600

    feat fix min height and font

--- src/app/chat-ai/page.tsx
+++ src/app/chat-ai/page.tsx
@@ -7,19 +7,19 @@ import { Separator } from '@/components/ui/separator'
 
 export default function Setup() {
   return (
-    <main className="min-w-dvw container mx-auto h-dvh w-full max-w-screen-xl bg-background font-robotoSlab">
+    <main className="min-w-dvw container mx-auto h-dvh w-full max-w-screen-xl bg-background px-2 font-pontanoSans sm:px-4">
       <div
         className={
-          'flex h-full flex-col-reverse md:flex md:h-full md:w-full md:flex-row md:flex-nowrap'
+          'flex h-full min-h-[40rem] flex-col-reverse lg:flex lg:h-full lg:w-full lg:flex-row lg:flex-nowrap'
         }
       >
-        <section className="relative h-2/6 md:h-full md:w-4/12">
+        <section className="relative h-1/6 min-h-[10rem] lg:h-full lg:w-4/12">
           <Menu />
           <Microphone />
-          <Separator className="absolute right-0 top-0 hidden md:grid" orientation="vertical" />
-          <Separator className="absolute right-0 top-0 grid md:hidden" orientation="horizontal" />
+          <Separator className="absolute right-0 top-0 hidden lg:grid" orientation="vertical" />
+          <Separator className="absolute right-0 top-0 grid lg:hidden" orientation="horizontal" />
         </section>
-        <section className="h-4/6 md:h-full md:w-8/12">
+        <section className="h-5/6 lg:h-full lg:w-8/12">
           <Chat />
         </section>
       </div>
