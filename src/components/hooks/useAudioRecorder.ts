dbfeee8e89cb850119c34d1ccbd91379244e731b
Iván Bautista <ivanbautistag@gmail.com>
2024-10-24 17:56:21 -0600

    refactor: Retrieve OpenAI response and speech assessments simultaneously

--- src/components/hooks/useAudioRecorder.ts
+++ src/components/hooks/useAudioRecorder.ts
@@ -71,7 +71,7 @@ export const useAudioRecorder = (): hookReturnType => {
         const stringId = Date.now().toString()
         const userContent = await transcribeAudio(audioBlobRecorded)
         const url = URL.createObjectURL(audioBlobRecorded)
-        await chatAi({ userContent, id: stringId, urlUsr: url })
+        chatAi({ userContent, id: stringId, urlUsr: url })
         await speechAssesment({
           audioBlob: audioBlobRecorded,
           referenceText: userContent,
