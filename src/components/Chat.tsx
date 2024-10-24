c8d9715911c35d00142823d9440056172e489189
Iván Bautista <ivanbautistag@gmail.com>
2024-10-24 16:48:53 -0600

    feat: add background to conversation chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use client'
 
 import { useAppStore } from '@/store/zustand-store'
@@ -43,26 +44,29 @@ export default function Chat() {
 
   return (
     <div
-      className="scrollbar-none flex h-full flex-col gap-4 overflow-y-auto scroll-smooth p-0 font-robotoSlab md:p-4"
+      className="scrollbar-none flex h-full flex-col gap-4 overflow-y-auto scroll-smooth p-0 font-pontanoSans md:p-4"
       ref={refContainer}
     >
       {conversation.map((message) => {
         const isNotIntroMsg = message.id !== 'start'
         const isUser = message.role === 'user'
-        const position = isUser ? 'justify-end' : 'justify-start'
-        const bg = isUser ? 'bg-[#73bda8]/80 text-stone-950' : 'bg-stone-900'
-        const textContent = isUser ? 'text-stone-800' : 'text-muted-foreground'
+        const position = isUser ? '  sm:justify-end ' : 'justify-start'
+        const bg = isUser ? ' text-stone-950' : 'bg-card'
+        const textContent = isUser ? 'text-stone-800' : 'text-primary'
         const contentObj = !isUser && isNotIntroMsg ? JSON.parse(message.content) : null
 
         return (
-          <div key={message.id} className={`flex w-full animate-in ${position}`}>
-            <Card className={`font-allan w-5/6 ${bg}`}>
+          <div key={message.id} className={`flex w-full animate-in ${position} `}>
+            <Card
+              className={`w-full font-pontanoSans sm:w-11/12 md:w-5/6 ${bg} min-w-[21.5rem] border-4 border-primary bg-[url('/images/cards.webp')] bg-cover bg-center bg-no-repeat bg-blend-color-burn`}
+            >
               <CardHeader className="p-2 pb-2">
-                <CardTitle>{message.role}</CardTitle>
+                <CardTitle className="px-4 text-left">{message.role}</CardTitle>
+                <Separator orientation="horizontal" className="flex p-0" />
                 {isNotIntroMsg && <AudioWave url={message.url} dark={isUser} autoPlay={!isUser} />}
-                <Separator orientation="horizontal" className="top-2 flex" />
+                {isNotIntroMsg && <Separator orientation="horizontal" className="my-2 flex" />}
               </CardHeader>
-              <CardContent className={`p-2 pt-0 font-robotoSlab ${textContent} `}>
+              <CardContent className={`p-2 pt-0 ${textContent} font-pontanoSans`}>
                 <ChatCardContent
                   contentObj={contentObj}
                   idAssesment={message.idAssesment}
@@ -70,7 +74,8 @@ export default function Chat() {
                 />
               </CardContent>
               {isUser && isAzureEnabled && (
-                <CardFooter className="flex animate-typingFade flex-col p-2 pt-0 font-robotoSlab">
+                <CardFooter className="flex animate-typingFade flex-col p-2 pt-0">
+                  <Separator orientation="horizontal" className="my-2 flex bg-primary/10" />
                   <SpeechAssessmentResult idAssessment={message.idAssesment} />
                 </CardFooter>
               )}
