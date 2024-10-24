2ac78da5e3556e4d8de84402b4b22b4aa30f000b
Iván Bautista <ivanbautistag@gmail.com>
2024-10-24 16:57:18 -0600

    refactor set min heights

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -2,8 +2,8 @@ import SetupForm from '@/components/SetupForm'
 
 export default function Setup() {
   return (
-    <main className="min-w-dvw relative h-dvh w-full flex-col flex-nowrap items-center justify-center bg-background font-robotoSlab">
-      <div className="flex h-full w-full flex-col items-center justify-center">
+    <main className="min-w-dvw relative h-dvh w-full flex-col flex-nowrap items-center justify-center bg-background font-pontanoSans">
+      <div className="flex h-full min-h-[40rem] w-full min-w-[20rem] flex-col items-center justify-center">
         <SetupForm />
       </div>
     </main>
