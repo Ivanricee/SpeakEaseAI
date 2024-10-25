c6e10d190a40ec64919355e87c2a3769c4c50871
Iván Bautista <ivanbautistag@gmail.com>
2024-10-24 18:08:43 -0600

    refactor: add min width

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -34,7 +34,7 @@ SheetOverlay.displayName = SheetPrimitive.Overlay.displayName
 const sheetVariants = cva(
   `fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out
   data-[state=open]:animate-in data-[state=closed]:animate-out
-  data-[state=closed]:duration-300 data-[state=open]:duration-500 overflow-y-auto`,
+  data-[state=closed]:duration-300 data-[state=open]:duration-500 overflow-y-auto min-w-full md:min-w-[35rem] `,
   {
     variants: {
       side: {
