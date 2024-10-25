d173af04f3a52a524dfae369d9fab5a7c5028e49
Iván Bautista <ivanbautistag@gmail.com>
2024-10-24 18:03:42 -0600

    refactor change width and height

--- src/components/ui/circularProgress.tsx
+++ src/components/ui/circularProgress.tsx
@@ -47,19 +47,19 @@ export default function CircularProgress({
   const progressOffset = timer ?? 30
   return (
     <div className={`flex h-full items-center justify-center ${className}`}>
-      <div className="relative h-[200px] w-[200px]">
+      <div className="relative h-[120px] w-[120px] lg:h-[200px] lg:w-[200px]">
         <svg
           className="absolute left-0 top-0 h-full w-full -rotate-90 transform"
           viewBox="0 0 100 100"
         >
-          <circle cx="50" cy="50" r="40" fill="transparent" stroke="#2b2b2b" strokeWidth="4" />
+          <circle cx="50" cy="50" r="40" fill="transparent" stroke="#838170" strokeWidth="4" />
           <circle
             className="transition-all duration-500 ease-in-out"
             cx="50"
             cy="50"
             r="40"
             fill="transparent"
-            stroke="#ddc715"
+            stroke="#4D4B42"
             strokeWidth="4"
             strokeDasharray="251.2"
             strokeDashoffset={251.2 * (1 - progressOffset / 30)}
