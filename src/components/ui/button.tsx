5fc08a2da5c2bea976806cb259fa40249024798b
Iván Bautista <ivanbautistag@gmail.com>
2024-10-24 18:02:23 -0600

    refactor: change color and font

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
@@ -11,14 +12,12 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        default: 'bg-accent text-accent-foreground hover:bg-teal-600/40',
+        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+          'w-min border-2 border-teal-700/60 bg-secondary font-bold text-teal-800 duration-500 hover:bg-accent/80 hover:text-accent-foreground/70 bg-accent/80',
+        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary text-teal-800',
+        ghost: 'hover:bg-accent/80 text-teal-800/90 hover:text-accent-foreground/70',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
@@ -45,11 +44,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
     return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
+      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
     )
   }
 )
