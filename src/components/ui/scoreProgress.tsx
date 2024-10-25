bb029a3eeb8656f53fa8091a4975806b5246f73e
Iván Bautista <ivanbautistag@gmail.com>
2024-10-24 18:07:08 -0600

    refactor: Update ticker progress bar

--- src/components/ui/scoreProgress.tsx
+++ src/components/ui/scoreProgress.tsx
@@ -15,19 +15,19 @@ export function ScoreProgress({ score, children }: { score: number; children?: R
   const strokeColor = getColor(score)
   return (
     <div className={'flex h-full items-center justify-center'}>
-      <div className="relative h-[30px] w-[30px]">
+      <div className="relative h-[35px] w-[35px]">
         <svg
           className="absolute left-0 top-0 h-full w-full -rotate-90 transform"
           viewBox="0 0 100 100"
         >
-          <circle cx="50" cy="50" r="40" fill="transparent" stroke="#2b2b2b" strokeWidth="18" />
+          <circle cx="50" cy="50" r="40" fill="transparent" stroke="#4D4B42" strokeWidth="18" />
           <circle
             className={`transition-all duration-500 ease-in-out ${strokeColor}`}
             cx="50"
             cy="50"
             r="40"
             fill="transparent"
-            strokeWidth="5"
+            strokeWidth="15"
             strokeDasharray="255"
             strokeDashoffset={255 * (1 - score / 100)}
           />
