3f027edb9e326840ee9ea7d32ca895254609bb24
Iván Bautista <ivanbautistag@gmail.com>
2024-10-24 18:07:30 -0600

    refactor: change background color

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -14,8 +14,8 @@ const Separator = React.forwardRef<
     decorative={decorative}
     orientation={orientation}
     className={cn(
-      'shrink-0 bg-border',
-      orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]',
+      'shrink-0 bg-primary',
+      orientation === 'horizontal' ? 'h-[2px] w-full' : 'h-full w-[2px]',
       className
     )}
     {...props}
